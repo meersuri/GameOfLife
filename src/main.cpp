002a74f19c6e62f1a526d250b6792cae3dfd9c9a
meersuri <meersuri@gmail.com>
2024-02-08 21:44:29 -0500

    Add glider and increase grid size + time steps

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,21 @@
 using namespace std::chrono_literals;
 
 int main() {
-    std::unique_ptr<Universe> universe = std::make_unique<DenseUniverse>(6, 6);
+    std::unique_ptr<Universe> universe = std::make_unique<DenseUniverse>(20, 20);
     std::vector<std::pair<int, int>> toad = {{2,2}, {2,3}, {2,4}, {3,1}, {3,2}, {3,3}};
     std::vector<std::pair<int, int>> bee_hive = {{2,1}, {1,2}, {1,3}, {2,4}, {3,2}, {3,3}};
-    for (const std::pair<int, int>& p: toad) {
+    std::vector<std::pair<int, int>> glider = {{2, 1}, {3, 2}, {3, 3}, {2, 3}, {1, 3}};
+    for (const std::pair<int, int>& p: glider) {
         universe->cell(p.first, p.second)->makeAlive();
     }
     GridPainter painter(universe->rowCount(), universe->colCount());
-    size_t time_steps = 10;
+    size_t time_steps = 50;
     painter.clear();
     for (int i = 0; i < time_steps; ++i) {
         for (int row = 0; row < universe->rowCount(); row++) {
             for (int col = 0; col < universe->colCount(); col++) {
                 if (universe->cell(row, col)->isAlive()) {
-                    painter.paint(row, col, 'o', Color::green);
+                    painter.paint(row, col, '*', Color::green);
                 }
                 else {
                     painter.paint(row, col, ' ', Color::black);
